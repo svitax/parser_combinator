11d0d4cc1f8162efe51dd91d2f6a9d15288545f6
Igor Barale <svitaxiom@gmail.com>
2021-03-24 15:58:33 -0700

    str parser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const str = s => {
+const str = s => targetString => {
+  if (targetString.startsWith(s)) {
+    // success
+    return s
+  }
+  throw new Error(`Tried to match ${s}, but got ${targetString.slice(0, 10)}...`)
 
 }
 
+const run = (parser, targetString) => {
+  return parser(targetString)
+}
+
 const parser = str('hello there')
+
+console.log(
+  run(parser, 'hello not correct there')
+)
